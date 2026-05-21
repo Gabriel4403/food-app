2010adec0b6d1affe4859b84313d0c4f2904946a
Onofrei Gabriel <onofreigabriel86@gmail.com>
2026-05-21 15:26:56 +0300

    fix pool config

--- backend/db.js
+++ backend/db.js
@@ -1,29 +1,15 @@
 import mysql from 'mysql2/promise';
 import 'dotenv/config';
 
-let pool;
-
-if (process.env.DATABASE_URL) {
-  pool = mysql.createPool({
-    uri: process.env.DATABASE_URL,
-    waitForConnections: true,
-    connectionLimit: 10,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0,
-    ssl: { rejectUnauthorized: false },
-  });
-} else {
-  pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0,
-  });
-}
+const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+  waitForConnections: true,
+  connectionLimit: 10,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 10000,
+  idleTimeout: 60000,
+  maxIdle: 10,
+  ssl: { rejectUnauthorized: false },
+});
 
 export default pool;
