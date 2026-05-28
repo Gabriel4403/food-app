c45bdf87c84ee6ceaf52583646fb69cde788f9f2
Onofrei Gabriel <onofreigabriel86@gmail.com>
2026-05-28 12:10:53 +0300

    test hardcode api url

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,7 +2,7 @@ import useHttp from "../hooks/useHttp";
 import ProductItem from "../components/ProductItem";
 import Error from "../components/Error";
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API_URL = 'https://food-app-production-2bcf.up.railway.app';
 const requestConfig = {};
 
 function Products() {
